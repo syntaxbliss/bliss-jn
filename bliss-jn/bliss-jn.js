c6b0195dc0117d257b9c84fa12605c6f958a7e35
syntaxbliss <santiago.osella@hotmail.com.ar>
2015-08-02 16:45:38 -0300

    Implementar M6502

--- bliss-jn/bliss-jn.js
+++ bliss-jn/bliss-jn.js
@@ -23,7 +23,7 @@ var ZN_FLAGS = [
 /* 1 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
 /* 2 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
 /* 3 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
-/* 4 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      3,      0,      0,      0,
+/* 4 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
 /* 5 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
 /* 6 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
 /* 7 */      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,
@@ -58,8 +58,8 @@ var OPCODE_OPERATION = [
 //          0      1      2      3      4      5      6      7      8      9      a      b      c      d      e      f
 /* 0 */  null, 'ORA',  null,  null,  null, 'ORA', 'ASL',  null, 'PHP', 'ORA', 'ASL',  null,  null, 'ORA', 'ASL',  null,
 /* 1 */ 'BPL', 'ORA',  null,  null,  null, 'ORA', 'ASL',  null, 'CLC', 'ORA',  null,  null,  null, 'ORA', 'ASL',  null,
-/* 2 */ 'JSR', 'AND', 'AND',  null, 'BIT',  null, 'ROL',  null, 'PLP', 'AND', 'ROL',  null, 'BIT', 'AND', 'ROL',  null,
-/* 3 */ 'BMI', 'AND', 'AND',  null,  null,  null, 'ROL',  null, 'SEC', 'AND',  null,  null,  null, 'AND', 'ROL',  null,
+/* 2 */ 'JSR', 'AND',  null,  null, 'BIT', 'AND', 'ROL',  null, 'PLP', 'AND', 'ROL',  null, 'BIT', 'AND', 'ROL',  null,
+/* 3 */ 'BMI', 'AND',  null,  null,  null, 'AND', 'ROL',  null, 'SEC', 'AND',  null,  null,  null, 'AND', 'ROL',  null,
 /* 4 */ 'RTI', 'EOR',  null,  null,  null, 'EOR', 'LSR',  null, 'PHA', 'EOR', 'LSR',  null, 'JMP', 'EOR', 'LSR',  null,
 /* 5 */ 'BVC', 'EOR',  null,  null,  null, 'EOR', 'LSR',  null,  null, 'EOR',  null,  null,  null, 'EOR', 'LSR',  null,
 /* 6 */ 'RTS', 'ADC',  null,  null,  null, 'ADC', 'ROR',  null, 'PLA', 'ADC', 'ROR',  null, 'JMP', 'ADC', 'ROR',  null,
@@ -68,10 +68,10 @@ var OPCODE_OPERATION = [
 /* 9 */ 'BCC', 'STA',  null,  null, 'STY', 'STA', 'STX',  null, 'TYA', 'STA', 'TXS',  null,  null, 'STA',  null,  null,
 /* a */ 'LDY', 'LDA', 'LDX',  null, 'LDY', 'LDA', 'LDX',  null, 'TAY', 'LDA', 'TAX',  null, 'LDY', 'LDA', 'LDX',  null,
 /* b */ 'BCS', 'LDA',  null,  null, 'LDY', 'LDA', 'LDX',  null, 'CLV', 'LDA', 'TSX',  null, 'LDY', 'LDA', 'LDX',  null,
-/* c */ 'CPY', 'CMP',  null,  null, 'CPY', 'CMP',  null,  null, 'INY', 'CMP', 'DEX',  null, 'CPY', 'CMP',  null,  null,
-/* d */ 'BNE', 'CMP',  null,  null,  null, 'CMP',  null,  null, 'CLD', 'CMP',  null,  null,  null, 'CMP',  null,  null,
-/* e */ 'CPX', 'SBC',  null,  null, 'CPX',  null,  null,  null, 'INX', 'SBC', 'NOP',  null, 'CPX', 'SBC', 'SBC',  null,
-/* f */ 'BEQ', 'SBC',  null,  null,  null,  null,  null,  null, 'SED', 'SBC',  null,  null,  null, 'SBC', 'SBC',  null
+/* c */ 'CPY', 'CMP',  null,  null, 'CPY', 'CMP', 'DEC',  null, 'INY', 'CMP', 'DEX',  null, 'CPY', 'CMP', 'DEC',  null,
+/* d */ 'BNE', 'CMP',  null,  null,  null, 'CMP', 'DEC',  null, 'CLD', 'CMP',  null,  null,  null, 'CMP', 'DEC',  null,
+/* e */ 'CPX', 'SBC',  null,  null, 'CPX', 'SBC', 'INC',  null, 'INX', 'SBC', 'NOP',  null, 'CPX', 'SBC', 'INC',  null,
+/* f */ 'BEQ', 'SBC',  null,  null,  null, 'SBC', 'INC',  null, 'SED', 'SBC',  null,  null,  null, 'SBC', 'INC',  null
 ];
 
 var OPCODE_MODE = [
@@ -88,10 +88,10 @@ var OPCODE_MODE = [
 /* 9 */  REL,  IYN, null, null,  ZPX,  ZPX,  ZPY, null,  IMP,  AYN,  IMP, null, null,  AXN, null, null,
 /* a */  IMM,  INX,  IMM, null,  ZEP,  ZEP,  ZEP, null,  IMP,  IMM,  IMP, null,  ABS,  ABS,  ABS, null,
 /* b */  REL,  INY, null, null,  ZPX,  ZPX,  ZPY, null,  IMP,  ABY,  IMP, null,  ABX,  ABX,  ABY, null,
-/* c */  IMM,  INX, null, null,  ZEP,  ZEP, null, null,  IMP,  IMM,  IMP, null,  ABS,  ABS, null, null,
-/* d */  REL,  INY, null, null, null,  ZPX, null, null,  IMP,  ABY, null, null, null,  ABX, null, null,
-/* e */  IMM,  INX, null, null,  ZEP,  ZEP, null, null,  IMP,  IMM,  IMP, null,  ABS,  ABS, null, null,
-/* f */  REL,  INY, null, null, null,  ZPX, null, null,  IMP,  ABY, null, null, null,  ABX, null, null
+/* c */  IMM,  INX, null, null,  ZEP,  ZEP,  ZEP, null,  IMP,  IMM,  IMP, null,  ABS,  ABS,  ABS, null,
+/* d */  REL,  INY, null, null, null,  ZPX,  ZPX, null,  IMP,  ABY, null, null, null,  ABX,  AXN, null,
+/* e */  IMM,  INX, null, null,  ZEP,  ZEP,  ZEP, null,  IMP,  IMM,  IMP, null,  ABS,  ABS,  ABS, null,
+/* f */  REL,  INY, null, null, null,  ZPX,  ZPX, null,  IMP,  ABY, null, null, null,  ABX,  AXN, null
 ];
 
 var OPCODE_CYCLES = [
@@ -102,16 +102,16 @@ var OPCODE_CYCLES = [
 /* 3 */ 2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0,
 /* 4 */ 6, 6, 0, 0, 0, 3, 5, 0, 3, 2, 2, 0, 3, 4, 6, 0,
 /* 5 */ 2, 5, 0, 0, 0, 4, 6, 0, 0, 4, 0, 0, 0, 4, 7, 0,
-/* 6 */ 6, 6, 0, 0, 0, 3, 5, 0, 4, 2, 2, 0, 0, 4, 6, 0,
+/* 6 */ 6, 6, 0, 0, 0, 3, 5, 0, 4, 2, 2, 0, 5, 4, 6, 0,
 /* 7 */ 2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0,
 /* 8 */ 0, 6, 0, 0, 3, 3, 3, 0, 2, 0, 2, 0, 4, 4, 4, 0,
 /* 9 */ 2, 6, 0, 0, 4, 4, 4, 0, 2, 5, 2, 0, 0, 5, 0, 0,
 /* a */ 2, 6, 2, 0, 3, 3, 3, 0, 2, 2, 2, 0, 4, 4, 4, 0,
 /* b */ 2, 5, 0, 0, 4, 4, 4, 0, 2, 4, 2, 0, 4, 4, 4, 0,
-/* c */ 2, 6, 0, 0, 3, 3, 0, 0, 2, 2, 2, 0, 4, 4, 0, 0,
-/* d */ 2, 5, 0, 0, 0, 4, 0, 0, 2, 0, 0, 0, 0, 4, 0, 0,
-/* e */ 2, 6, 0, 0, 3, 3, 0, 0, 2, 2, 2, 0, 4, 4, 0, 0,
-/* f */ 2, 5, 0, 0, 0, 4, 0, 0, 2, 4, 0, 0, 0, 4, 0, 0
+/* c */ 2, 6, 0, 0, 3, 3, 5, 0, 2, 2, 2, 0, 4, 4, 6, 0,
+/* d */ 2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0,
+/* e */ 2, 6, 0, 0, 3, 3, 5, 0, 2, 2, 2, 0, 4, 4, 6, 0,
+/* f */ 2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0
 ];
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -134,7 +134,7 @@ BlissJN.NES = function( data, target ) {
 BlissJN.NES.prototype = {
     start : function() {
         var cycles = 0;
-        for( var i = 0; i < 1000; i++ ) {
+        for( var i = 0; i < 6000; i++ ) {
             var opcode = this.m6502.fetch();
             var c = this.m6502.execute( opcode );
             if( c === 0 ) throw "error: no se definieron los ciclos para el opcode < " + opcode.toHex(2) + " >";
@@ -215,8 +215,6 @@ BlissJN.NES.M6502.prototype = {
     },
 
     execute : function( opcode ) {
-        // FIXME
-        //console.log( opcode.toHex(2), this.pc.toHex(4) );
         this.debugger.trace( this.getContext(), opcode );
         this.ticks = 0;
         var operation = OPCODE_OPERATION[ opcode ];
@@ -240,14 +238,52 @@ BlissJN.NES.M6502.prototype = {
 
         switch( mode ) {
             case ABS: {
-                var l = this.fetch();
-                var h = this.fetch();
+                var l = this.fetch(), h = this.fetch();
                 address = ( (h << 8) | l ) & 0xffff;
+            } break;
+
+            case ABX: { // TODO: page-crossing
+                var l = this.fetch(), h = this.fetch();
+                address = ( ((h << 8) | l) + this.x ) & 0xffff;
+            } break;
+
+            case ABY: { // TODO: page-crossing
+                var l = this.fetch(), h = this.fetch();
+                address = ( ((h << 8) | l) + this.y ) & 0xffff;
+            } break;
+
+            case AXN: {
+                var l = this.fetch(), h = this.fetch();
+                address = ( ((h << 8) | l) + this.x ) & 0xffff;
+            } break;
+
+            case AYN: {
+                var l = this.fetch(), h = this.fetch();
+                address = ( ((h << 8) | l) + this.y ) & 0xffff;
             } break;
 
             case IMM: {
                 address = this.pc;
                 this.pc = ( this.pc + 1 ) & 0xffff;
+            } break;
+
+            case IND: {
+                var l  = this.fetch(), h = this.fetch();
+                var rl = this.mmu.readByte( ((h << 8) | l) & 0xffff ), rh = this.mmu.readByte( ((h << 8) | ((l + 1) & 0xff)) & 0xffff );
+                address = ( (rh << 8) | rl ) & 0xffff;
+            } break;
+
+            case INX: {
+                var r = ( this.fetch() + this.x ) & 0xff;
+                var l = this.mmu.readByte( r ), h = this.mmu.readByte( (r + 1) & 0xff );
+                address = ( (h << 8) | l ) & 0xffff;
+            } break;
+
+            case IYN: // TODO: page-crossing
+            case INY: {
+                var r = this.fetch();
+                var l = this.mmu.readByte( r ), h = this.mmu.readByte( (r + 1) & 0xff );
+                address = ( ((h << 8) | l) + this.y ) & 0xffff;
             } break;
 
             case IMP: {
@@ -256,6 +292,14 @@ BlissJN.NES.M6502.prototype = {
 
             case ZEP: case REL: {
                 address = this.fetch();
+            } break;
+
+            case ZPX: {
+                address = ( this.fetch() + this.x ) & 0xff;
+            } break;
+
+            case ZPY: {
+                address = ( this.fetch() + this.y ) & 0xff;
             } break;
 
             default: {
@@ -408,6 +452,14 @@ BlissJN.NES.M6502.prototype = {
         this.status |= ZN_FLAGS[ src ];
     },
 
+    DEC : function( mode ) {
+        var address = this.getAddress( mode );
+        var r       = ( this.mmu.readByte(address) - 1 ) & 0xff;
+        this.mmu.writeByte( address, r );
+        this.status &= ~( FLAG_Z | FLAG_N );
+        this.status |= ZN_FLAGS[ r ];
+    },
+
     DEX : function( mode ) {
         this.x = ( this.x - 1 ) & 0xff;
         this.status &= ~( FLAG_Z | FLAG_N );
@@ -424,6 +476,14 @@ BlissJN.NES.M6502.prototype = {
         this.a ^= this.mmu.readByte( this.getAddress(mode) );
         this.status &= ~( FLAG_Z | FLAG_N );
         this.status |= ZN_FLAGS[ this.a ];
+    },
+
+    INC : function( mode ) {
+        var address = this.getAddress( mode );
+        var r       = ( this.mmu.readByte(address) + 1 ) & 0xff;
+        this.mmu.writeByte( address, r );
+        this.status &= ~( FLAG_Z | FLAG_N );
+        this.status |= ZN_FLAGS[ r ];
     },
 
     INX : function( mode ) {
@@ -522,7 +582,7 @@ BlissJN.NES.M6502.prototype = {
         this.status &= ~( FLAG_C | FLAG_Z | FLAG_N );
         if( carry ) this.status |= FLAG_C;
         this.status |= ZN_FLAGS[ r ];
-        if( this.mode !== ACC ) this.mmu.writeByte( address, r ); else this.a = r;
+        if( mode !== ACC ) this.mmu.writeByte( address, r ); else this.a = r;
     },
 
     ROR : function( mode ) {
@@ -537,7 +597,7 @@ BlissJN.NES.M6502.prototype = {
         this.status &= ~( FLAG_C | FLAG_Z | FLAG_N );
         if( carry ) this.status |= FLAG_C;
         this.status |= ZN_FLAGS[ r ];
-        if( this.mode !== ACC ) this.mmu.writeByte( address, r ); else this.a = r;
+        if( mode !== ACC ) this.mmu.writeByte( address, r ); else this.a = r;
     },
 
     RTI : function( mode ) {
@@ -650,11 +710,11 @@ BlissJN.NES.M6502.Debugger.prototype = {
     operands : function( mode ) {
         var str = null;
         switch( mode ) {
-            case ABS: {
+            case ABS: case ABX: case ABY: case AXN: case AYN: case IND: {
                 str = this.peek(1).toHex(2) + " " + this.peek(2).toHex(2);
             } break;
 
-            case IMM: case REL: case ZEP: {
+            case IMM: case INX: case INY: case IYN: case REL: case ZEP: case ZPX: case ZPY: {
                 str = this.peek(1).toHex(2) + "   ";
             } break;
 
@@ -670,20 +730,59 @@ BlissJN.NES.M6502.Debugger.prototype = {
         str = name + ' ';
         switch( mode ) {
             case ABS: {
-                var l = this.peek(1), h = this.peek(2);
-                var address = ( (h << 8) | l ) & 0xffff;
+                var l = this.peek(1), h = this.peek(2), address = ( (h << 8) | l ) & 0xffff;
                 str += '$' + address.toHex(4);
-                if( ['JMP', 'JSR'].indexOf(name) === -1 ) str += ' = ' + this.context.mmu.readByte( address ).toHex(2) + '                  ';
+                if(['JMP', 'JSR'].indexOf(name) === -1 ) str += ' = ' + this.context.mmu.readByte( address ).toHex(2) + '                  ';
                 else str += '                       ';
             } break;
 
+            case ABX: { // TODO: page-crossing
+                var l = this.peek(1), h = this.peek(2), address = ( (h << 8) | l ) & 0xffff, disp = ( address + this.context.x ) & 0xffff;
+                str += '$' + address.toHex(4) + ',X @ ' + disp.toHex(4) + ' = ' + this.context.mmu.readByte( disp ).toHex(2) + '         ';
+            } break;
+
+            case ABY: { // TODO: page-crossing
+                var l = this.peek(1), h = this.peek(2), address = ( (h << 8) | l ) & 0xffff, disp = ( address + this.context.y ) & 0xffff;
+                str += '$' + address.toHex(4) + ',Y @ ' + disp.toHex(4) + ' = ' + this.context.mmu.readByte( disp ).toHex(2) + '         ';
+            } break;
+
+            case AXN: {
+                var l = this.peek(1), h = this.peek(2), address = ( (h << 8) | l ) & 0xffff, disp = ( address + this.context.x ) & 0xffff;
+                str += '$' + address.toHex(4) + ',X @ ' + disp.toHex(4) + ' = ' + this.context.mmu.readByte( disp ).toHex(2) + '         ';
+            } break;
+
+            case AYN: {
+                var l = this.peek(1), h = this.peek(2), address = ( (h << 8) | l ) & 0xffff, disp = ( address + this.context.y ) & 0xffff;
+                str += '$' + address.toHex(4) + ',Y @ ' + disp.toHex(4) + ' = ' + this.context.mmu.readByte( disp ).toHex(2) + '         ';
+            } break;
+
             case ACC: {
-                src += 'A                           ';
+                str += 'A                           ';
             } break;
 
             case IMM: {
                 str += '#$' + this.peek(1).toHex(2);
                 str += '                        ';
+            } break;
+
+            case IND: {
+                var l = this.peek(1), h = this.peek(2), address = ( (h << 8) | l ) & 0xffff;
+                var rl = this.context.mmu.readByte( address ), rh = this.context.mmu.readByte( (address + 1) & 0xffff );
+                str += '($' + address.toHex(4) + ') = ' + ( (rh << 8) | rl ).toHex(4) + '              ';
+            } break;
+
+            case INX: {
+                var r = this.peek(1), disp = ( r + this.context.x ) & 0xff;
+                var l = this.context.mmu.readByte( disp ), h = this.context.mmu.readByte( (disp + 1) & 0xff );
+                var address = ( (h << 8) | l ) & 0xffff, data = this.context.mmu.readByte( address );
+                str += '($' + r.toHex(2) + ',X) @ ' + disp.toHex(2) + ' = ' + address.toHex(4) + ' = ' + data.toHex(2) + '    ';
+            } break;
+
+            case IYN: // TODO: page-crossing
+            case INY: {
+                var r = this.peek(1), l = this.context.mmu.readByte( r ), h = this.context.mmu.readByte( (r + 1) & 0xff );
+                var address = ( (h << 8) | l ) & 0xffff, disp = ( address + this.context.y ) & 0xffff, data = this.context.mmu.readByte( disp );
+                str += '($' + r.toHex(2) + '),Y = ' + address.toHex(4) + ' @ ' + disp.toHex(4) + ' = ' + data.toHex(2) + '  ';
             } break;
 
             case IMP: {
@@ -699,6 +798,16 @@ BlissJN.NES.M6502.Debugger.prototype = {
             case ZEP: {
                 str += '$' + this.peek(1).toHex(2) + " = " + this.context.mmu.readByte( this.peek(1) ).toHex(2);
                 str += '                    ';
+            } break;
+
+            case ZPX: {
+                var r = this.peek(1), disp = ( r + this.context.x ) & 0xff;
+                str += '$' + r.toHex(2) + ',X @ ' + disp.toHex(2) + ' = ' + this.context.mmu.readByte( disp ).toHex(2) + '             ';
+            } break;
+
+            case ZPY: {
+                var r = this.peek(1), disp = ( r + this.context.y ) & 0xff;
+                str += '$' + r.toHex(2) + ',Y @ ' + disp.toHex(2) + ' = ' + this.context.mmu.readByte( disp ).toHex(2) + '             ';
             } break;
         }
         return str;
